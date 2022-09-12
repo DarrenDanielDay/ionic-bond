f77b991122aacefdc5bcc38d9abd5de37b7621c9
DarrenDanielDay <Darren_Daniel_Day@hotmail.com>
2022-09-13 04:58:24 +0800

    feat: add major implementaions and tests

---
+++ src/index.spec.ts
@@ -0,0 +1,78 @@
+import { solvent } from "./index";
+describe("bond", () => {
+  it("should work like this", () => {
+    interface Nested {
+      a: {
+        b: {
+          c: number;
+        };
+        d: {
+          e: {
+            f: string;
+          }[];
+        };
+      };
+    }
+    const idx = 2;
+    const b = solvent<Nested>().ionze((n) => n.a.d.e[idx].f);
+    const obj: Nested = {
+      a: {
+        b: { c: 1 },
+        d: {
+          e: [{ f: "aaa" }, { f: "bbb" }, { f: "ccc" }],
+        },
+      },
+    };
+    const newObj = b.dissolve(obj).crystallize("fff");
+    // should create a new object
+    expect(newObj).not.toBe(obj);
+    // should preserve not updated parts
+    expect(newObj.a.b).toBe(obj.a.b);
+    expect(newObj.a.d.e[0]).toBe(obj.a.d.e[0]);
+    expect(newObj.a.d.e[1]).toBe(obj.a.d.e[1]);
+    // should update the changed parts
+    expect(newObj).toStrictEqual({
+      a: {
+        b: { c: 1 },
+        d: {
+          e: [{ f: "aaa" }, { f: "bbb" }, { f: "fff" }],
+        },
+      },
+    });
+  });
+
+  it("should throw error for invalid usage", () => {
+    expect(() => {
+      solvent<{ fn: () => void }>()
+        .ionze((a) => a.fn)
+        .dissolve({ fn() {} });
+    }).toThrow(/function/i);
+    expect(() => {
+      solvent<string[]>()
+        .ionze((a) => a[0].length)
+        .dissolve(["aaa"]);
+    }).toThrow(/primitive/i);
+    expect(() => {
+      solvent<{ tag?: string }>()
+        .ionze((a) => a.tag)
+        .dissolve({});
+    }).toThrow(/unknown prop/i);
+    expect(() => {
+      solvent<string[]>()
+        .ionze((a) => {
+          const arr = [a[0]];
+          arr.pop();
+          return "aaa";
+        })
+        .dissolve(["aaa"]);
+    }).toThrow(/bond lost/i);
+    expect(() => {
+      class A {
+        b = 1;
+      }
+      solvent<A>()
+        .ionze((a) => a.b)
+        .dissolve(new A());
+    }).toThrow(/class/i);
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,74 @@
-export {}
+export interface Ionization<S extends Salt, I> {
+  (salt: S): I;
+}
+export type Salt = object;
+export type Bond = PropertyKey;
+export type Suspension = [Salt, Salt, Bond];
+export type Solution<S extends Salt, I> = {
+  crystallize(this: void, ion: I): S;
+};
+
+export const solvent = <S extends Salt>() => ({
+  ionze: <I>(ionization: Ionization<S, I>) => {
+    return {
+      dissolve: (salt: S) => {
+        return {
+          crystallize: dissolveWith(salt, ionization),
+        };
+      },
+    };
+  },
+});
+const isPrimitive: unique symbol = Symbol("isPrimitive");
+const dissolveWith = <S extends Salt, I>(salt: S, ionization: Ionization<S, I>) => {
+  const suspensions = new WeakMap<Salt, Suspension>();
+  const createProxy = <T extends Salt>(target: T): T => {
+    if (typeof target === "function") {
+      throw new Error("Cannot call function in ionization.");
+    }
+    if (!(typeof target === "object" && target)) {
+      // @ts-expect-error Dynamic Implementation
+      return createProxy({
+        [isPrimitive]: isPrimitive,
+      });
+    }
+
+    return new Proxy(target, {
+      get(proxyTarget, property, receiver) {
+        if (Reflect.has(target, isPrimitive)) {
+          throw new Error("Cannot chain bond with primitive properties.");
+        }
+        if (!(property in proxyTarget)) {
+          throw new Error("Invalid ionization. Unknown properties are not allowed.");
+        }
+        const accessed = Reflect.get(proxyTarget, property);
+        const newProxy = createProxy(accessed);
+        suspensions.set(newProxy, [receiver, proxyTarget, property]);
+        return newProxy;
+      },
+    });
+  };
+  const proxy = createProxy(salt);
+  // @ts-expect-error Dynamic Implementation
+  const located: object = ionization(proxy);
+  let suspension = suspensions.get(located);
+  if (!suspension) {
+    throw new Error("Ionic bond lost.");
+  }
+  const crystalizeDirectives: ((ion: unknown) => unknown)[] = [];
+  while (suspension) {
+    const [nucleus, cluster, bond] = suspension;
+    if (Array.isArray(cluster)) {
+      crystalizeDirectives.push((ion) => Object.assign(new Array(cluster.length), cluster, { [bond]: ion }));
+    } else {
+      const prototype = Object.getPrototypeOf(cluster);
+      if (prototype === null || Object.is(prototype, Object.prototype)) {
+        crystalizeDirectives.push((ion) => Object.assign({}, cluster, { [bond]: ion }));
+      } else {
+        throw new Error("Class instance is currently not supported. Only simple object and array are supported.");
+      }
+    }
+    suspension = suspensions.get(nucleus);
+  }
+  return (ion: I): S => crystalizeDirectives.reduce<any>((previousSalt, directive) => directive(previousSalt), ion);
+};
